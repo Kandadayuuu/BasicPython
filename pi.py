769d4198f3041253169eefeedc7617cd66ee8b87
Yusuke <y.kanda.24.4.1397@4nonome.com>
2024-05-13 08:48:00 +0900

    Python入門　問2　解答

--- pi.py
+++ pi.py
@@ -1,7 +1,2 @@
-text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
-"""
-
-# TODO
-
+text="How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
+print(int("".join(map(str, list(map(len, text.split()))))))
