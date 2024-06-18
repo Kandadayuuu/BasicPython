207da995685a1747eadc6e1974bfe914c30cbb14
Yusuke <y.kanda.24.4.1397@4nonome.com>
2024-06-18 14:56:37 +0900

    制御構文問2の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,15 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+#hは幅 kは個数
+import math
+h=(math.pi/2)/100
+k=1 
+#Sは全体の面積
+S=0
+#s="k個目の台形の面積"
+while k<=100:
+    s=(h/2)*((sin((k-1)*h))+(sin(k*h)))
+    S +=s
+    k += 1
+print(S)
