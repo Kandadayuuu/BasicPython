74aff883f6eb7efd3b96f4e4034369e0dcf250a9
Yusuke <y.kanda.24.4.1397@4nonome.com>
2024-06-18 14:57:01 +0900

    制御構文問3の解答

--- euclid.py
+++ euclid.py
@@ -1,4 +1,40 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+#(1)
+a = 10
+b = 20
 
 # TODO
+if a>b:
+    while b!=0:
+        a, b = b, a % b      
+    print(f"最大公約数: {a}")
+if a<=b:
+    while a!=0:
+        b, a = a, b % a
+    print(f"最大公約数: {b}")
+#(2)
+a = 14
+b = 91
+
+# TODO
+if a>b:
+    while b!=0:
+        a, b = b, a % b      
+    print(f"最大公約数: {a}")
+if a<=b:
+    while a!=0:
+        b, a = a, b % a
+    print(f"最大公約数: {b}")
+
+#(3)
+a = 91
+b = 14
+
+# TODO
+if a>b:
+    while b!=0:
+        a, b = b, a % b      
+    print(f"最大公約数: {a}")
+if a<=b:
+    while a!=0:
+        b, a = a, b % a
+    print(f"最大公約数: {b}")
